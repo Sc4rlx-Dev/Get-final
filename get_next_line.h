8df229f8e613387fa314da0e6ef99ab4ebf752c4
Sc4rlx-Dev <itsscarl2022@gmail.com>
2024-03-23 15:56:19 +0000

    commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: oer-raou <oer-raou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/21 15:43:30 by oer-raou          #+#    #+#             */
-/*   Updated: 2024/03/21 16:46:33 by oer-raou         ###   ########.fr       */
+/*   Updated: 2024/03/23 15:14:49 by oer-raou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,20 @@
 
 /* get_next_line */
 
-#include <stdlib.h> // For malloc, free
-#include <unistd.h> // For read
-#include <string.h> // For strchr, strdup
+#include <stdlib.h>
+#include <unistd.h> 
 #include <fcntl.h>
-#include <stdio.h>
+#include <stdio.h> 
 /*
  ** =========[ Defines ]===========
  */
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 1
 # endif
 
 # ifndef OPEN_MAX
-#  define OPEN_MAX 1024
+#  define OPEN_MAX 1024 
 # endif
 
 /*
@@ -47,14 +46,6 @@ char	*ft_strdup(char const *s1);
 char	*ft_strjoin(char *s1, char const *s2);
 char	*ft_substr(char const *str, unsigned int start, size_t size);
 
-/*
- ** get_next_line.c
- **
- ** Read each line of the given file, one at a function call.
- */
-
-/* static char *ft_next(char **temp) */
-/* static char *ft_read(char *temp, int fd) */
 char	*get_next_line(int fd);
 
 #endif
